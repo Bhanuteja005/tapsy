add4a6ffb110a007a752afe044d5d597b4f87af2
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-07-02 22:38:09 +0530

    bhanu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios'; // Import axios
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -10,30 +9,16 @@ import Task from "./pages/Task";
 import { saveProfile } from "./redux/actions/authActions";
 
 function App() {
+
   const authState = useSelector(state => state.authReducer);
   const dispatch = useDispatch();
-
-  // State for form inputs
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // Set axios defaults
-  axios.defaults.withCredentials = true;
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('glomera-backend.vercel.app', { name, email, password })
-      .then(result => console.log(result))
-      .catch(err => console.log(err));
-  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) return;
     dispatch(saveProfile(token));
   }, [authState.isLoggedIn, dispatch]);
+
 
   return (
     <>
