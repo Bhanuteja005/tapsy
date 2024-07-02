4e720a1e95c18c9854c1cb62120e38f4b61dfc79
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-07-02 22:25:41 +0530

    bhanu

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from 'react-router-dom';
 import validateManyFields from '../validations';
 import Input from './utils/Input';
-import { useDispatch, useSelector } from "react-redux";
-import { postLoginData } from '../redux/actions/authActions';
 import Loader from './utils/Loader';
-import { useEffect } from 'react';
-
 const LoginForm = ({ redirectUrl }) => {
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const errors = validateManyFields("login", formData);
+    setFormErrors({});
+    if (errors.length > 0) {
+      setFormErrors(errors.reduce((total, ob) => ({ ...total, [ob.field]: ob.err }), {}));
+      return;
+    }
+    axios.defaults.withCredentials = true;
+    axios.post('glomera-backend.vercel.app', {
+      email: formData.email,
+      password: formData.password
+    })
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+  }
 
   const [formErrors, setFormErrors] = useState({});
   const [formData, setFormData] = useState({
@@ -35,16 +51,7 @@ const LoginForm = ({ redirectUrl }) => {
     });
   }
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const errors = validateManyFields("login", formData);
-    setFormErrors({});
-    if (errors.length > 0) {
-      setFormErrors(errors.reduce((total, ob) => ({ ...total, [ob.field]: ob.err }), {}));
-      return;
-    }
-    dispatch(postLoginData(formData.email, formData.password));
-  }
+
 
 
 
@@ -57,32 +64,32 @@ const LoginForm = ({ redirectUrl }) => {
 
   return (
     <>
-      <form className='m-auto my-16 max-w-[500px] bg-white p-8 border-2 shadow-md rounded-md'>
-        {loading ? (
-          <Loader />
-        ) : (
-          <>
-            <h2 className='text-center mb-4'>Welcome user, please login here</h2>
-            <div className="mb-4">
-              <label htmlFor="email" className="after:content-['*'] after:ml-0.5 after:text-red-500">Email</label>
-              <Input type="text" name="email" id="email" value={formData.email} placeholder="youremail@domain.com" onChange={handleChange} />
-              {fieldError("email")}
-            </div>
+      <form className='m-auto my-16 max-w-[500px] bg-white p-8 border-2 shadow-md rounded-md' onSubmit={handleSubmit}>
+  {loading ? (
+    <Loader />
+  ) : (
+    <>
+      <h2 className='text-center mb-4'>Welcome user, please login here</h2>
+      <div className="mb-4">
+        <label htmlFor="email" className="after:content-['*'] after:ml-0.5 after:text-red-500">Email</label>
+        <Input type="text" name="email" id="email" value={formData.email} placeholder="youremail@domain.com" onChange={handleChange} />
+        {fieldError("email")}
+      </div>
 
-            <div className="mb-4">
-              <label htmlFor="password" className="after:content-['*'] after:ml-0.5 after:text-red-500">Password</label>
-              <Input type="password" name="password" id="password" value={formData.password} placeholder="Your password.." onChange={handleChange} />
-              {fieldError("password")}
-            </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="after:content-['*'] after:ml-0.5 after:text-red-500">Password</label>
+        <Input type="password" name="password" id="password" value={formData.password} placeholder="Your password.." onChange={handleChange} />
+        {fieldError("password")}
+      </div>
 
-            <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark' onClick={handleSubmit}>Submit</button>
+      <button type='submit' className='bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark'>Submit</button>
 
-            <div className='pt-4'>
-              <Link to="/signup" className='text-blue-400'>Don't have an account? Signup here</Link>
-            </div>
-          </>
-        )}
-      </form>
+      <div className='pt-4'>
+        <Link to="/signup" className='text-blue-400'>Don't have an account? Signup here</Link>
+      </div>
+    </>
+  )}
+</form>
     </>
   )
 }
